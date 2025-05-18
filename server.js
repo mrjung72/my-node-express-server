a09ecb3c3c157db8f1165e55b9bba171d396e5b7
sahara <mrjung72@gmail.com>
2025-05-18 20:45:55 +0900

    csv파일 업로드 기능 추가

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // server.js
 const express = require('express');
+const multer = require('multer');
+const csv = require('csv-parser');
+const fs = require('fs');
+const mysql = require('mysql2');
+
 const cors = require('cors');
 const pool = require('./db');
 require('dotenv').config();
@@ -9,6 +14,7 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+
 
 // Get all users
 app.get('/api/users', async (req, res) => {
@@ -36,6 +42,56 @@ app.post('/api/register', async (req, res) => {
     res.status(500).send('Failed to insert user');
   }
 });
+
+
+// multer 설정
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/api/upload-csv', upload.single('file'), async (req, res) => {
+    const filePath = req.file.path
+
+    const results = []
+    const insertedRows = [];
+
+    fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', async () => {
+            try {
+                const conn = await pool.getConnection()
+
+                for (const row of results) {
+                    // 유효성 검사 및 중복 체크 예시
+                    if (!row.username || !row.email) {
+                        console.log('유효하지 않은 데이터:', row)
+                        continue
+                    }
+
+                    const exists = await conn.query('SELECT id FROM users WHERE email = ?', [row.email] )
+
+                    if (exists[0].length === 0) {
+                        await conn.query(
+                            'INSERT INTO users(username, password, email) VALUES(?, ?, ?)',
+                            [row.username, row.password, row.email]
+                        )
+                        insertedRows.push(1);
+                    }
+                    else {
+                        console.log('중복된 데이터:', row)
+                    } 
+                }
+
+                conn.release()
+                fs.unlinkSync(filePath) // 업로드된 파일 삭제
+                res.json({ message: `총 ${insertedRows.length}건의 데이터가 업로드되었습니다.!!!`})
+            } catch (err) {
+                console.error(err)
+                res.status(500).json({ message: '서버 오류' })
+            }
+        })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
