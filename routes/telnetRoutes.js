63f3f66a2b00805ce646ef1c6315c5d1865ba488
sahara <mrjung72@gmail.com>
2025-06-03 17:02:22 +0900

    멀티 서버 체크 기능 추가가

--- routes/telnetRoutes.js
+++ routes/telnetRoutes.js
@@ -3,7 +3,44 @@ const net = require('net')
 const express = require('express')
 const router = express.Router()
 
-router.post('/telnet', async (req, res) => {
+
+router.post('/bulk', async (req, res) => {
+  const targets = req.body // [{ ip, port }]
+  if (!Array.isArray(targets)) return res.status(400).json({ error: '형식 오류' })
+
+  const timeout = 3000
+
+  const checkTelnet = ({ ip, port }) =>
+    new Promise(resolve => {
+      const socket = new net.Socket()
+      let status = 'timeout'
+      console.log(ip, port)
+
+      socket.setTimeout(timeout)
+      socket.connect(port, ip, () => {
+        status = 'success'
+        socket.destroy()
+        resolve({ ip, port, status })
+      })
+
+      socket.on('error', () => {
+        status = 'error'
+        socket.destroy()
+        resolve({ ip, port, status })
+      })
+
+      socket.on('timeout', () => {
+        socket.destroy()
+        resolve({ ip, port, status })
+      })
+    })
+
+  const results = await Promise.all(targets.map(checkTelnet))
+  res.json(results)
+})
+
+
+router.post('/single', async (req, res) => {
 
   const { ip, port } = req.body
   if (!ip || !port) return res.status(400).json({ error: 'IP 또는 포트가 누락되었습니다.' })
