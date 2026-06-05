f69e40f798666a2e3636799dc0659328efb2c4f8
정형택 <mrjung72@gmail.com>
2026-06-05 21:03:16 +0900

    Google cound run 환경에서 frontend/backend 연동을 위한 설정 추가

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(cors())
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : true,
+  credentials: true
+}
+app.use(cors(corsOptions))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
